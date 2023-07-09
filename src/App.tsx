ea245cadcb55021d5dfc7e08b7c4cc3ceb1b2d86
Cruz Salas <cruz.danielss@icloud.com>
2023-07-08 18:09:18 -0600

    working state no styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import logo from "./logo.svg";
+import "./App.css";
+import Die, { DieProps } from "./components/Die/Die";
 
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  const diceElements: any = dice.map((die) => (
+    <Die
+      id={die.id}
+      isHeld={die.isHeld}
+      value={die.value}
+      holdDice={() => holdDice(die.id)}
+    />
+  ));
+
+  function allNewDice() {
+    const diceArr = [];
+    for (let i = 0; i <= 4; i++) {
+      diceArr.push(generateNewDice());
+    }
+    return diceArr;
+  }
+
+  function generateNewDice() {
+    return {
+      isHeld: false,
+      value: Math.ceil(Math.random() * 6),
+      id: nanoid(),
+    };
+  }
+
+  function holdDice(id: string) {
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      });
+    });
+  }
+
+  function rollDice() {
+    setDice((oldDice) => {
+      return oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDice();
+      });
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="title">Testing</h1>
+      <h3 className="instructions">testiong</h3>
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={rollDice}>button</button>
+    </main>
   );
 }
 
